f20ce6888c90451064d1a988a31b0dd1d7669492
ChrisUtada <liulidao@gmail.com>
2026-06-13 14:05:25 +0800

    refactor: remove programmatic SFX, keep external-ready API with resource paths

--- autoload/sfx.gd
+++ autoload/sfx.gd
@@ -1,15 +1,20 @@
 extends Node
 
+@export var click_path: String = "res://assets/sfx/click.ogg"
+@export var drop_path: String = "res://assets/sfx/drop.ogg"
+@export var button_path: String = "res://assets/sfx/button.ogg"
+@export var success_path: String = "res://assets/sfx/success.ogg"
+
 var _click: AudioStreamPlayer
 var _drop: AudioStreamPlayer
 var _button: AudioStreamPlayer
 var _success: AudioStreamPlayer
 
 func _ready():
-	_click = _player(_mk_click(1500, 0.03))
-	_drop = _player(_mk_click(800, 0.05))
-	_button = _player(_mk_click(1200, 0.025))
-	_success = _player(_mk_click(2000, 0.06))
+	_click = _player(_load(click_path))
+	_drop = _player(_load(drop_path))
+	_button = _player(_load(button_path))
+	_success = _player(_load(success_path))
 
 	EventBus.card_drag_started.connect(func(_c): play_click())
 	EventBus.card_placed_in_slot.connect(func(_s, _c): play_drop())
@@ -18,6 +23,11 @@ func _ready():
 	EventBus.card_broken.connect(func(_c): play_drop())
 	EventBus.card_combined.connect(func(_b, _t, _r): play_success())
 
+func _load(path: String) -> AudioStream:
+	if ResourceLoader.exists(path):
+		return load(path)
+	return null
+
 func _player(stream: AudioStream) -> AudioStreamPlayer:
 	var p = AudioStreamPlayer.new()
 	p.stream = stream
@@ -25,33 +35,17 @@ func _player(stream: AudioStream) -> AudioStreamPlayer:
 	return p
 
 func play_click():
-	_click.play()
+	if _click.stream:
+		_click.play()
 
 func play_drop():
-	_drop.play()
+	if _drop.stream:
+		_drop.play()
 
 func play_button():
-	_button.play()
+	if _button.stream:
+		_button.play()
 
 func play_success():
-	_success.play()
-
-static func _mk_click(freq: float, dur: float) -> AudioStreamWAV:
-	var rate = 22050
-	var n = int(rate * dur)
-	var data = PackedByteArray()
-	data.resize(n)
-	for i in range(n):
-		var t = float(i) / rate
-		var env = exp(-t * 120.0)
-		var s = sin(t * TAU * freq) * env * 0.4 + sin(t * TAU * freq * 1.5) * env * 0.15
-		data[i] = int(clamp((s + 1.0) * 0.5 * 255, 0, 255))
-	return _finalize(data, rate)
-
-static func _finalize(data: PackedByteArray, rate: int) -> AudioStreamWAV:
-	var w = AudioStreamWAV.new()
-	w.format = AudioStreamWAV.FORMAT_8_BITS
-	w.stereo = false
-	w.mix_rate = rate
-	w.data = data
-	return w
+	if _success.stream:
+		_success.play()
